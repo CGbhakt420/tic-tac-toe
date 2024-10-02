2b687ca1cd493a9b89bc70885a74ba31bd2d21bd
CGbhakt420 <sanchit.kr007@gmail.com>
2024-10-02 13:25:19 +0530

    added player markers on the gameboard

--- script.js
+++ script.js
@@ -7,11 +7,23 @@ const Gameboard = (()=>{
             boardHTML += `<div class="square" id="square-${index}">${square}</div>`
         })
         document.querySelector("#gameboard").innerHTML = boardHTML;
+        const squares = document.querySelectorAll(".square");
+        squares.forEach((square)=>{
+            square.addEventListener("click", Game.handleClick);
+        })
+        
+    }
+
+    const update = (index, value) => {
+        gboard[index] = value;
+        render();
+
     }
     
+    const getGBoard = () => gboard;
 
     return{
-        render
+        render, update, getGBoard
     }
 
 })();
@@ -32,14 +44,47 @@ const Game = (()=>{
         Gameboard.render();
 
     }
+
+    const handleClick = (event) =>{
+        let index = parseInt(event.target.id.split("-")[1]);
+        //alert(index);
+
+        if(Gameboard.getGBoard()[index] !== ""){
+            return;
+        }
+        Gameboard.update(index, players[currentplayerindex].mark);
+
+        if(checkforWin(Gameboard.getGBoard(), players[currentplayerindex].mark)){
+            gameover = true;
+            alert(`${players[currentplayerindex].name} won!`)
+        }
+
+        currentplayerindex = currentplayerindex ===0 ? 1 : 0;
+
+    }
+
+    const restart = ()=>{
+        for(let i=0; i<9; i++){
+            Gameboard.update(i, "");
+        }
+        Gameboard.render();
+    }
+
     return{
-        start
+        start, handleClick, restart
     }
     
 })();
 
 
+
+
 const startButton = document.querySelector("#start-button");
 startButton.addEventListener("click", ()=>{
     Game.start();
+})
+
+const restartButton = document.querySelector("#restart-button");
+restartButton.addEventListener("click", ()=>{
+    Game.restart();
 })
